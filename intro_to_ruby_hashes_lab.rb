559d0901013d6f839c58cef95d79282eb2981b9a
Mark Cascardi <mcascardi@gmail.com>
2020-04-17 17:37:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,15 +12,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads =>
+	monopoly = {
+		:railroads => 
 		{
-			:pieces => 4,
-			{
-				:names => {}
-			},
-			{
-				:rent_in_dollars => {}
-			}
+			:pieces => 4, 
+			:names => {},
+			:rent_in_dollars => {}
 		}
 }
 
