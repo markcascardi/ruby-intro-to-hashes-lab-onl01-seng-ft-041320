26c2a492aaf03edbcc9b605a5ca8d46ce94a79f2
Mark Cascardi <mcascardi@gmail.com>
2020-04-17 17:45:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,11 +30,23 @@ def monopoly_with_third_tier
 			:pieces => 4,
 			:names =>
 			{
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {}
+				:reading_railroad => 
+				{
+					
 				},
+				:pennsylvania_railroad => 
+				{
+					
+				},
+				:b_and_o_railroad => 
+				{
+					
+				},
+				:shortline => 
+				{
+					
+				}
+			},
 			:rent_in_dollars =>
 			{
 				:one_piece_owned => 25,
